b286e5dc6ee8e683f4e82d212991c08270847b3f
lunker <dk910320@gmail.com>
2015-02-22 23:25:45 +0900

    update

--- app/src/main/java/dev/dain/sms/SmsReceiver.java
+++ app/src/main/java/dev/dain/sms/SmsReceiver.java
@@ -1,13 +1,24 @@
 package dev.dain.sms;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
 import java.util.Date;
+
+import dev.dain.MainActivity;
+import dev.dain.R;
 
 /**
  * Created by lunker on 15. 2. 22..
@@ -16,17 +27,14 @@ public class SmsReceiver extends BroadcastReceiver {
 
 
     private final String TAG = "dain";
-
+    private Context context = null;
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
+        this.context = context;
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
             // 부팅완료
-
-
         }
-
 
         if ("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) {
             // sms 수신
@@ -50,6 +58,45 @@ public class SmsReceiver extends BroadcastReceiver {
 // SMS 메시지 확인
             String message = smsMessage[0].getMessageBody().toString();
             Log.v(TAG,"문자 내용"+ "발신자 : "+origNumber+", 내용 : " + message);
+
+            if(message.contains("투썸") || message.contains("할리스")){
+
+                Handler handle = new Handler();
+                handle.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+                        makeNotification();
+                    }
+                }, 900);
+            }
+
         }
+    }// end receive
+
+
+    public void makeNotification(){
+        Log.v(TAG,"in make noti");
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);//required
+
+//                mBuilder.setLargeIcon(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.dain), 72,72,false));
+        mBuilder.setLargeIcon(BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.dain));
+        mBuilder.setContentTitle("Dain");//required
+        mBuilder.setContentText("평가해보세요 ! ");//required
+        mBuilder.setTicker("커피를 구매하셨나요? ");//optional
+        mBuilder.setNumber(10);//optional
+        mBuilder.setAutoCancel(true);
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
     }
+
+
 }
